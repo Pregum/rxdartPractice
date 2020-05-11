8300973fa3ead738118496cde259b6000aeb5b0b
Pregum <pregum.y@gmail.com>
2020-05-11 23:19:01 +0900

    concatEagerとconcatをブログに合わせて更新

--- lib/rxdart_concat_sample.dart
+++ lib/rxdart_concat_sample.dart
@@ -1,8 +1,8 @@
 import 'package:rxdart/rxdart.dart';
 
 void studyConcat() {
-  // concatは実行中のObservableが完了するまで、次のObservableが購読されないため
-  // 'do on listen.'は3つ目のObservableが完了してから表示されます。
+  // concatは実行中のStreamが完了するまで、次のStreamが購読されないため
+  // 'do on listen.'は3つ目のStreamが完了してから表示されます。
   // timerを使用してもその秒数待った後、順番に出力されます。
   Rx.concat<int>([
     Stream<int>.value(1),
@@ -13,6 +13,12 @@ void studyConcat() {
         .doOnListen(() => print('do on listen.')),
   ]).listen((i) => print('listen: $i'));
 }
+// 実行結果
+// listen: 1
+// listen: 2
+// listen: 3
+// do on listen.
+// listen: 4
 
 void studyConcatStream() {
   ConcatStream<int>([
@@ -23,3 +29,10 @@ void studyConcatStream() {
         .transform(DoStreamTransformer(onListen: () => print('do on listen.'))),
   ]).listen((i) => print('listen: $i'));
 }
+// 実行結果
+// listen: 1
+// listen: 2
+// listen: 3
+// do on listen.
+// listen: 4
+
