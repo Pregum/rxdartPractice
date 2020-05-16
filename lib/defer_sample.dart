e02b87464767f5c86733fc8c4df29e49cc5d41f9
Pregum <pregum.y@gmail.com>
2020-05-16 10:37:27 +0900

    [update] defer_sample.dartのコメント等を0.23.0対応するよう修正

--- lib/defer_sample.dart
+++ lib/defer_sample.dart
@@ -1,6 +1,6 @@
 import 'package:rxdart/rxdart.dart';
 
-// reusable=trueのObservable<T>.defer
+// reusable=trueのRx.defer<T>
 // rxdartのDeferオペレータ
 void studyDeferWithReusable() async {
   var count = 0;
@@ -11,13 +11,18 @@ void studyDeferWithReusable() async {
   }, reusable: true);
 
   deferSubscription.listen((str) => print('listen1: $str'));
-  // 複数Observableが発行されていることがわかるよう少し待機
+  // 複数Streamが発行されていることがわかるよう少し待機
   await Future.delayed(Duration(milliseconds: 10));
   // 二人目の購読者を登録してもエラーは発生しない
   deferSubscription.listen((str) => print('listen2: $str'));
 }
+// 実行結果
+// hello world: 1
+// listen1: defer push.
+// hello world: 2
+// listen2: defer push.
 
-// reuasable=falseのObservable<T>.defer
+// reuasable=falseのRx.defer<T>
 void studyDefer() async {
   var count = 0;
   // reusable引数をしていないため、reusableにはfalseが設定されている
@@ -28,11 +33,14 @@ void studyDefer() async {
   });
 
   deferSubscription.listen((str) => print('listen1: $str'));
-  // 複数Observableが発行されていることがわかるよう少し待機
+  // 複数Streamが発行されていることがわかるよう少し待機
   await Future.delayed(Duration(milliseconds: 10));
   // 下記の行をコメントアウトして実行するとエラーが発生する。
   // deferSubscription.listen((str) => print('listen2: $str'));
 }
+// 実行結果
+// hello world: 1
+// listen1: defer push.
 
 // Stream APIのDeferオペレータ
 void studyDeferStreamWithReusable() async {
@@ -43,11 +51,16 @@ void studyDeferStreamWithReusable() async {
   }, reusable: true);
 
   deferSubscription.listen((str) => print('listen1: $str'));
-  // 複数Observableが発行されていることがわかるよう少し待機
+  // 複数Streamが発行されていることがわかるよう少し待機
   await Future.delayed(Duration(milliseconds: 10));
   // 二人目の購読者を登録してもエラーは発生しない
   deferSubscription.listen((str) => print('listen2: $str'));
 }
+// 実行結果
+// hello world: 1
+// listen1: defer push.
+// hello world: 2
+// listen2: defer push.
 
 void studyDeferStream() async {
   var count = 0;
@@ -59,8 +72,11 @@ void studyDeferStream() async {
   });
 
   deferSubscription.listen((str) => print('listen1: $str'));
-  // 複数Observableが発行されていることがわかるよう少し待機
+  // 複数Streamが発行されていることがわかるよう少し待機
   await Future.delayed(Duration(milliseconds: 10));
   // 下記の行をコメントアウトするとエラーが発生する。
   // deferSubscription.listen((str) => print('listen2: $str'));
 }
+// 実行結果
+// hello world: 1
+// listen1: defer push.
